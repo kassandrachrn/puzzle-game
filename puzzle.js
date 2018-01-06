32d1dbb0b72877261530c7f2d65d09ece10a4728
Costi <costi@google.com>
2018-01-06 09:41:08 +0200

    created swapping functionality

--- puzzle.js
+++ puzzle.js
@@ -4,16 +4,10 @@
 var imageToCut = new Image();
 var canvas;
 var pieces = [];
-var BB;
-var offsetX;
-var offsetY;
-var WIDTH;
-var HEIGHT;
+var pieceOne;
+var pieceTwo;
+var clickIsPressed = false;
 
-// drag related variables
-var dragok = false;
-var startX;
-var startY;
 
 $(function () {
      $(document)
@@ -83,18 +77,76 @@ function splitImage(){
 			for(var j=0;j<lines;j++){
                     context.drawImage(imageToCut, j*stepH, i*stepV, stepH, stepV,
                         j*(stepH+space), i*(stepV+space), stepH, stepV);
-                          pieces.push(context.getImageData(j*(stepH+space),i*(stepV+space),stepH,stepV));
+                          pieces.push({
+                            left:j*(stepH+space),
+                            top:i*(stepV+space),
+                            width:stepH,
+                            height:stepV,
+                            imageData: context.getImageData(j*(stepH+space),i*(stepV+space),stepH,stepV)
+                          });
 		                    }
     }
+
     document.getElementById("p").innerHTML = "Move and connect the pieces by clicking them";
 
-    BB = canvas.getBoundingClientRect();
-    offsetY = BB.top;
-    offsetX = BB.left;
-    WIDTH = canvas.width;
-    HEIGHT = canvas.height;
 
+    canvas.addEventListener('click', function(event) {
 
+    var rect = canvas.getBoundingClientRect();
+
+    var scaleX = canvas.width / rect.width;   // relationship bitmap vs. element for X
+    var scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for
+
+    var x = (event.clientX - rect.left) * scaleX;
+    var y = (event.clientY - rect.top) * scaleY;
+
+    // Collision detection between clicked offset and element.
+    pieces.forEach(function(element) {
+      if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
+
+            if(clickIsPressed==false){
+              pieceOne=element;
+              clickIsPressed = true;
+            }else{
+              console.log("clicked 2");
+              pieceTwo = element;
+              clickIsPressed = false;
+              swappingPieces(context,pieceOne,pieceTwo);
+            }
+      }
+    });
+
+  }, false);
+
+    console.log(pieces);
 }
 
 // 3. selecting objects
+
+function swappingPieces(context,pieceOne,pieceTwo){
+
+  let tempLeftOne = pieceOne.left;
+  let tempTopOne = pieceOne.top;
+
+  let tempLeftTwo = pieceTwo.left;
+  let tempTopTwo = pieceTwo.top;
+
+  context.putImageData(pieceOne.imageData,pieceTwo.left,pieceTwo.top);
+  context.putImageData(pieceTwo.imageData,pieceOne.left,pieceOne.top);
+
+  pieceOne.left = tempLeftTwo;
+  pieceOne.top = tempTopTwo;
+
+  pieceTwo.left = tempLeftOne;
+  pieceTwo.top = tempTopOne;
+
+  // Swapp positions in array;
+  let tempPieceOne = pieceOne;
+  let tempPieceTwo = pieceTwo;
+
+  pieces[pieces.indexOf(pieceOne)] = tempPieceTwo;
+  pieces[pieces.indexOf(pieceTwo)] = tempPieceOne;
+
+}
+
+// 4. Randomize puzzle
